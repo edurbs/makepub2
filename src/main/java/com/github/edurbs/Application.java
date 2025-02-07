3930118c9a263237ba3a9220bd253a1f3c5cadde
Eduardo Soares <edurbs@gmail.com>
2025-02-07 18:53:09 -0300

    del 2 columns conversor

--- src/main/java/com/github/edurbs/Application.java
+++ src/main/java/com/github/edurbs/Application.java
@@ -18,5 +18,7 @@ public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        
     }
 }
